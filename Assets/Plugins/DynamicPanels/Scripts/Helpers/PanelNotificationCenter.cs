858b1d06137815b793b34e0446181f73c1914bb6
0thElement <quangquangtrung89@gmail.com>
2022-12-18 20:26:02 +0700

    Compose: Install UnityDynamicPanels
    - Modify to work with TMPro and MaterialIcon

---
+++ Assets/Plugins/DynamicPanels/Scripts/Helpers/PanelNotificationCenter.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicPanels
+{
+	public static class PanelNotificationCenter
+	{
+		internal static class Internal
+		{
+			public static void PanelCreated( Panel panel )
+			{
+				if( !IsPanelRegistered( panel ) )
+				{
+					panels.Add( panel );
+
+					panel.Internal.ChangeCloseButtonVisibility( m_onPanelClosed != null );
+
+					if( OnPanelCreated != null )
+						OnPanelCreated( panel );
+
+					if( panel.gameObject.activeInHierarchy )
+					{
+						if( OnPanelBecameActive != null )
+							OnPanelBecameActive( panel );
+					}
+					else
+					{
+						if( OnPanelBecameInactive != null )
+							OnPanelBecameInactive( panel );
+					}
+				}
+			}
+
+			public static void PanelDestroyed( Panel panel )
+			{
+				if( panels.Remove( panel ) && OnPanelDestroyed != null )
+					OnPanelDestroyed( panel );
+			}
+
+			public static void PanelBecameActive( Panel panel )
+			{
+				if( IsPanelRegistered( panel ) )
+				{
+					if( OnPanelBecameActive != null )
+						OnPanelBecameActive( panel );
+				}
+			}
+
+			public static void PanelBecameInactive( Panel panel )
+			{
+				if( IsPanelRegistered( panel ) )
+				{
+					if( OnPanelBecameInactive != null )
+						OnPanelBecameInactive( panel );
+				}
+			}
+
+			public static void PanelClosed( Panel panel )
+			{
+				if( m_onPanelClosed != null )
+					m_onPanelClosed( panel );
+			}
+
+			public static void TabDragStateChanged( PanelTab tab, bool isDragging )
+			{
+				if( isDragging )
+				{
+					if( OnStartedDraggingTab != null )
+						OnStartedDraggingTab( tab );
+				}
+				else
+				{
+					if( OnStoppedDraggingTab != null )
+						OnStoppedDraggingTab( tab );
+				}
+			}
+
+			public static void ActiveTabChanged( PanelTab tab )
+			{
+				if( OnActiveTabChanged != null )
+					OnActiveTabChanged( tab );
+			}
+
+			public static void TabIDChanged( PanelTab tab, string previousID, string newID )
+			{
+				if( !idToTab.ContainsValue( tab ) )
+				{
+					tab.Internal.ChangeCloseButtonVisibility( m_onTabClosed != null );
+
+					if( OnTabCreated != null )
+						OnTabCreated( tab );
+				}
+
+				if( !string.IsNullOrEmpty( previousID ) )
+				{
+					PanelTab previousTab;
+					if( idToTab.TryGetValue( previousID, out previousTab ) && previousTab == tab )
+						idToTab.Remove( previousID );
+				}
+
+				if( !string.IsNullOrEmpty( newID ) )
+					idToTab[newID] = tab;
+				else if( OnTabDestroyed != null )
+					OnTabDestroyed( tab );
+			}
+
+			public static void TabClosed( PanelTab tab )
+			{
+				if( m_onTabClosed != null )
+					m_onTabClosed( tab );
+			}
+
+			private static bool IsPanelRegistered( Panel panel )
+			{
+				for( int i = panels.Count - 1; i >= 0; i-- )
+				{
+					if( panels[i] == panel )
+						return true;
+				}
+
+				return false;
+			}
+		}
+
+		public delegate void PanelDelegate( Panel panel );
+		public delegate void TabDelegate( PanelTab tab );
+
+		public static event PanelDelegate OnPanelCreated, OnPanelDestroyed, OnPanelBecameActive, OnPanelBecameInactive;
+		public static event TabDelegate OnTabCreated, OnTabDestroyed, OnActiveTabChanged, OnStartedDraggingTab, OnStoppedDraggingTab;
+
+		private static PanelDelegate m_onPanelClosed;
+		public static event PanelDelegate OnPanelClosed
+		{
+			add
+			{
+				if( value != null )
+				{
+					if( m_onPanelClosed == null )
+					{
+						for( int i = panels.Count - 1; i >= 0; i-- )
+							panels[i].Internal.ChangeCloseButtonVisibility( true );
+					}
+
+					m_onPanelClosed += value;
+				}
+			}
+			remove
+			{
+				if( value != null && m_onPanelClosed != null )
+				{
+					m_onPanelClosed -= value;
+
+					if( m_onPanelClosed == null )
+					{
+						for( int i = panels.Count - 1; i >= 0; i-- )
+							panels[i].Internal.ChangeCloseButtonVisibility( false );
+					}
+				}
+			}
+		}
+
+		private static TabDelegate m_onTabClosed;
+		public static event TabDelegate OnTabClosed
+		{
+			add
+			{
+				if( value != null )
+				{
+					if( m_onTabClosed == null )
+					{
+						foreach( PanelTab tab in idToTab.Values )
+							tab.Internal.ChangeCloseButtonVisibility( true );
+					}
+
+					m_onTabClosed += value;
+				}
+			}
+			remove
+			{
+				if( value != null && m_onTabClosed != null )
+				{
+					m_onTabClosed -= value;
+
+					if( m_onTabClosed == null )
+					{
+						foreach( PanelTab tab in idToTab.Values )
+							tab.Internal.ChangeCloseButtonVisibility( false );
+					}
+				}
+			}
+		}
+
+		private static readonly List<Panel> panels = new List<Panel>( 32 );
+		public static int NumberOfPanels { get { return panels.Count; } }
+
+		private static readonly Dictionary<string, PanelTab> idToTab = new Dictionary<string, PanelTab>( 32 );
+
+		public static Panel GetPanel( int panelIndex )
+		{
+			if( panelIndex >= 0 && panelIndex < panels.Count )
+				return panels[panelIndex];
+
+			return null;
+		}
+
+		public static bool TryGetTab( string tabID, out PanelTab tab )
+		{
+			if( string.IsNullOrEmpty( tabID ) )
+			{
+				tab = null;
+				return false;
+			}
+
+			return idToTab.TryGetValue( tabID, out tab );
+		}
+	}
+}
---
+++ Assets/Plugins/DynamicPanels/Scripts/Helpers/PanelResizeHelper.cs
@@ -0,0 +1,139 @@
+﻿#if UNITY_EDITOR || ( !UNITY_ANDROID && !UNITY_IOS )
+#define ENABLE_CURSOR_MANAGEMENT
+#endif
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DynamicPanels
+{
+	[DisallowMultipleComponent]
+	public class PanelResizeHelper : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+#if ENABLE_CURSOR_MANAGEMENT
+		, IPointerEnterHandler, IPointerExitHandler
+#endif
+	{
+		private Panel m_panel;
+		public Panel Panel { get { return m_panel; } }
+
+		public RectTransform RectTransform { get; private set; }
+
+		private Direction m_direction;
+		private Direction secondDirection;
+
+		public Direction Direction { get { return m_direction; } }
+
+		private PanelResizeHelper helperBefore, helperAfter;
+
+		private int pointerId = PanelManager.NON_EXISTING_TOUCH;
+
+		private void Awake()
+		{
+			RectTransform = (RectTransform) transform;
+		}
+
+		private void OnEnable()
+		{
+			pointerId = PanelManager.NON_EXISTING_TOUCH;
+		}
+
+		public void Initialize( Panel panel, Direction direction, PanelResizeHelper helperBefore, PanelResizeHelper helperAfter )
+		{
+			m_panel = panel;
+
+			this.m_direction = direction;
+			this.helperBefore = helperBefore;
+			this.helperAfter = helperAfter;
+		}
+
+#if ENABLE_CURSOR_MANAGEMENT
+		public void OnPointerEnter( PointerEventData eventData )
+		{
+			PanelCursorHandler.OnPointerEnter( this, eventData );
+		}
+
+		public void OnPointerExit( PointerEventData eventData )
+		{
+			PanelCursorHandler.OnPointerExit( this );
+		}
+#endif
+
+		public void OnBeginDrag( PointerEventData eventData )
+		{
+			// Cancel drag event if panel is already being resized by another pointer
+			// or panel is anchored to a fixed anchor in that direction
+			if( !m_panel.CanResizeInDirection( m_direction ) )
+			{
+				eventData.pointerDrag = null;
+				return;
+			}
+
+			pointerId = eventData.pointerId;
+			secondDirection = GetSecondDirection( eventData.pressPosition );
+
+#if ENABLE_CURSOR_MANAGEMENT
+			PanelCursorHandler.OnBeginResize( m_direction, secondDirection );
+#endif
+		}
+
+		public void OnDrag( PointerEventData eventData )
+		{
+			if( eventData.pointerId != pointerId )
+				return;
+
+			m_panel.Internal.OnResize( m_direction, eventData.position );
+
+			if( secondDirection != Direction.None )
+				m_panel.Internal.OnResize( secondDirection, eventData.position );
+		}
+
+		public void OnEndDrag( PointerEventData eventData )
+		{
+			if( eventData.pointerId != pointerId )
+				return;
+
+			if( !m_panel.IsDocked )
+				( (UnanchoredPanelGroup) m_panel.Group ).RestrictPanelToBounds( m_panel );
+
+			pointerId = PanelManager.NON_EXISTING_TOUCH;
+
+#if ENABLE_CURSOR_MANAGEMENT
+			PanelCursorHandler.OnEndResize();
+#endif
+		}
+
+		public Direction GetSecondDirection( Vector2 pointerPosition )
+		{
+			if( m_panel.IsDocked )
+				return Direction.None;
+
+			Direction result;
+			if( RectTransformUtility.RectangleContainsScreenPoint( helperBefore.RectTransform, pointerPosition, m_panel.Canvas.Internal.worldCamera ) )
+				result = helperBefore.m_direction;
+			else if( RectTransformUtility.RectangleContainsScreenPoint( helperAfter.RectTransform, pointerPosition, m_panel.Canvas.Internal.worldCamera ) )
+				result = helperAfter.m_direction;
+			else
+				result = Direction.None;
+
+			if( !m_panel.CanResizeInDirection( result ) )
+				result = Direction.None;
+
+			return result;
+		}
+
+		public void Stop()
+		{
+			if( pointerId != PanelManager.NON_EXISTING_TOUCH )
+			{
+				if( !m_panel.IsDocked )
+					( (UnanchoredPanelGroup) m_panel.Group ).RestrictPanelToBounds( m_panel );
+
+				pointerId = PanelManager.NON_EXISTING_TOUCH;
+
+#if ENABLE_CURSOR_MANAGEMENT
+				PanelCursorHandler.OnEndResize();
+#endif
+			}
+		}
+	}
+}
