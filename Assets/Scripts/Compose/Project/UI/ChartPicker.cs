a51d9b5762d69441130962dec000e07df84226da
0thElement <quangquangtrung89@gmail.com>
2023-01-02 00:55:02 +0700

    Compose: Add a proper chart selection menu
    - Add option to remove chart from project
    - Todo: add a proper confirmation dialog for removing chart

---
+++ Assets/Scripts/Compose/Project/UI/ChartPicker.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ArcCreate.Compose.Project
+{
+    public class ChartPicker : MonoBehaviour
+    {
+        [SerializeField] private GameObject chartRowPrefab;
+        [SerializeField] private Transform rowsParent;
+        [SerializeField] private GameObject chartPicker;
+        [SerializeField] private Button togglePickerButton;
+        [SerializeField] private Button newChartButton;
+        [SerializeField] private NewChartDialog newChartDialog;
+        private readonly List<ChartPickerRow> chartPickerRows = new List<ChartPickerRow>();
+
+        public void SetOptions(List<ChartSettings> charts, ChartSettings currentChart)
+        {
+            foreach (ChartPickerRow row in chartPickerRows)
+            {
+                Destroy(row.gameObject);
+            }
+
+            chartPickerRows.Clear();
+
+            foreach (ChartSettings chart in charts)
+            {
+                GameObject go = Instantiate(chartRowPrefab, rowsParent);
+                ChartPickerRow row = go.GetComponent<ChartPickerRow>();
+                row.SetChart(chart);
+                row.SetCurrentlyActive(currentChart == chart);
+                chartPickerRows.Add(row);
+            }
+        }
+
+        private void Awake()
+        {
+            togglePickerButton.onClick.AddListener(TogglePicker);
+            newChartButton.onClick.AddListener(OpenNewChartDialog);
+        }
+
+        private void OnDestroy()
+        {
+            togglePickerButton.onClick.RemoveListener(TogglePicker);
+            newChartButton.onClick.RemoveListener(OpenNewChartDialog);
+        }
+
+        private void TogglePicker()
+        {
+            chartPicker.SetActive(!chartPicker.activeSelf);
+        }
+
+        private void OpenNewChartDialog()
+        {
+            newChartDialog.Open();
+        }
+    }
+}
