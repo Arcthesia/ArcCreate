f7427126e717b30416ed68d0eb8f14ea120d82ca
0thElement <quangquangtrung89@gmail.com>
2023-01-27 16:22:16 +0700

    Compose: Add text dialog

--- Assets/Scripts/Compose/Popups/TextDialog/TextDialog.cs
+++ Assets/Scripts/Compose/Popups/TextDialog/TextDialog.cs
@@ -1,14 +1,22 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ArcCreate.Compose.Popups
 {
+    [RequireComponent(typeof(RectTransform))]
     public class TextDialog : MonoBehaviour
     {
+        private RectTransform rect;
         [SerializeField] private TMP_Text titleText;
         [SerializeField] private TMP_Text contentText;
+        [SerializeField] private RectTransform contentRect;
         [SerializeField] private GameObject buttonPrefab;
         [SerializeField] private Transform buttonParent;
+        [SerializeField] private float extraHeight;
+        [SerializeField] private float maxHeight;
+        [SerializeField] private Color[] buttonColors;
 
         public void Setup(string title, string content, ButtonSetting[] buttonSettings)
         {
@@ -19,13 +27,31 @@ namespace ArcCreate.Compose.Popups
             {
                 GameObject go = Instantiate(buttonPrefab, buttonParent);
                 TextDialogButton button = go.GetComponent<TextDialogButton>();
-                button.Setup(setting.Text, setting.Callback, this);
+                Color color = buttonColors[(int)setting.ButtonColor];
+                button.Setup(setting.Text, setting.Callback, color, this);
             }
+
+            StartCoroutine(SetBoxHeightCoroutine());
         }
 
         public void CloseSelf()
         {
             Destroy(gameObject);
+        }
+
+        // I hate unity so much
+        private IEnumerator SetBoxHeightCoroutine()
+        {
+            yield return null;
+            float preferredHeight = contentRect.rect.height;
+            rect.SetSizeWithCurrentAnchors(
+                RectTransform.Axis.Vertical,
+                Mathf.Min(maxHeight, preferredHeight + extraHeight));
+        }
+
+        private void Awake()
+        {
+            rect = GetComponent<RectTransform>();
         }
     }
 }
